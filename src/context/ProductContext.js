c7a1cf2e494d936aabfffd60d86273e51bc55910
programmerNizam <mdnizam12109@gmail.com>
2022-12-27 15:42:25 +0600

    product context connect with app

--- src/context/ProductContext.js
+++ src/context/ProductContext.js
@@ -1,17 +1,23 @@
-import React, { createContext, useEffect, useState } from 'react';
+import React, { createContext, useContext, useEffect, useState } from 'react';
 const PRODUCT_CONTEXT = createContext()
-const ProductContext = () => {
+const ProductContext = ({ children }) => {
     const [product, setProduct] = useState([])
     useEffect(() => {
-        fetch("http://localhost:5000/products").then(res => res.json()).then(data => setProduct(data.data))
+        fetch("http://localhost:5000/products")
+            .then(res => res.json())
+            .then(data => setProduct(data.data))
     }, [])
-    const values = {
+    const value = {
         product
     }
     return (
-        <PRODUCT_CONTEXT.Provider value={values}>
+        <PRODUCT_CONTEXT.Provider value={value}>
+            {children}
         </PRODUCT_CONTEXT.Provider>
     );
 };
-
+export const useProducts = () => {
+    const context = useContext(PRODUCT_CONTEXT)
+    return context
+}
 export default ProductContext;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,9 @@
 import React from "react";
+import { useProducts } from "../context/ProductContext";
 
 const Home = () => {
-
+  const { product } = useProducts()
+  console.log(product);
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
       <h1>This is home page</h1>
